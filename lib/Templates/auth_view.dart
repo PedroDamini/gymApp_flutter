5ffde10d59701198262c61d8d1ca881896dd81f0
Pedro de Lemos Damini <pedrodelemosdamini@gmail.com>
2023-12-15 15:02:34 -0300

    ajuste no formulario de autentificação

--- lib/Templates/auth_view.dart
+++ lib/Templates/auth_view.dart
@@ -32,62 +32,71 @@ class _AuthViewState extends State<AuthView> {
           Padding(
             padding: const EdgeInsets.all(16),
             child: Form(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Image.asset("assets/logo.png", height: 128,),
-                  const Text(
-                    "GymApp",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 38,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 32,
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      label: Text("E-mail"),
-                    ),
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      label: Text("Senha"),
-                    ),
-                    obscureText: true,
-                  ),
-                  Visibility(
-                    visible: !queroEntrar,
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            label: Text("Confirme Senha"),
-                          ),
-                          obscureText: true,
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Image.asset("assets/logo.png", height: 128,),
+                      const Text(
+                        "GymApp",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 38,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
-                      ],
-                    ),
-                  )
-                  const SizedBox(
-                    height: 20,
+                      ),
+                      const SizedBox(
+                        height: 32,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          label: Text("E-mail"),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          label: Text("Senha"),
+                        ),
+                        obscureText: true,
+                      ),
+                      Visibility(
+                        visible: !queroEntrar,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                label: Text("Confirme Senha"),
+                              ),
+                              obscureText: true,
+                            ),
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                label: Text("Nome"),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text((queroEntrar)? "Entrar": "Cadastrar"),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            queroEntrar = !queroEntrar;
+                          });
+                        },
+                        child: Text((queroEntrar)? "Ainda não tem uma conta? Cadastre-se" : "Já tem uma conta? Entre"),)
+                    ],
                   ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text((queroEntrar)? "Entrar": "Cadastrar"),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      setState(() {
-                        queroEntrar = !queroEntrar;
-                      });
-                    },
-                    child: Text((queroEntrar)? "Ainda não tem uma conta? Cadastre-se" : "Já tem uma conta? Entre"),)
-                ],
+                ),
               ),
             ),
           ),
