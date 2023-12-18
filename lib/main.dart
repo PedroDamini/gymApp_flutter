0c15fd7471448a59424595a7aba99bd481e7bc84
Pedro de Lemos Damini <pedrodelemosdamini@gmail.com>
2023-12-18 15:57:59 -0300

    configurando firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutte_gym_app/Templates/auth_view.dart';
 import 'package:flutte_gym_app/Templates/exercise_view.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -16,4 +22,5 @@ class MyApp extends StatelessWidget {
       home: AuthView(),
     );
   }
+  
 }
